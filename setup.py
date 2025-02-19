c9dbb014aa7ca2e4ef651bf7844a71570b31fd72
pollx <pollxx97@gmail.com>
2025-02-19 16:14:58 +0300

    1) Added config.json, 2) replace accept float to accept int

--- setup.py
+++ setup.py
@@ -3,9 +3,38 @@ from cx_Freeze import setup, Executable
 
 # Указываем пакеты, которые нужно включить
 build_options = {
-    "packages": ["pandas", "scipy", "customtkinter", "matplotlib"],  # Перечислите здесь ваши пакеты
-    "excludes": ["pytest", "black", "pyright", ""],  # Пакеты, которые нужно исключить
-    "include_files": ["config.json"]  # Внешние файлы (например, изображения, конфиги)
+    "packages": [
+        "pandas",
+        "scipy",
+        "customtkinter",
+        "matplotlib",
+    ],  # Перечислите здесь ваши пакеты
+    "excludes": [
+        "pytest",
+        "black",
+        "pyright",
+        "scipy-stubs",
+        "pandas-stubs",
+        "pytest-mypy",
+        "matplotlib-stubs",
+        "cx-freeze",
+        # "scipy.linalg",
+        "scipy.fft",
+        "scipy.signal",
+        # "scipy.sparse",
+        "scipy.integrate",
+        "scipy.io",
+        "scipy.interpolate",
+        "scipy.ndimage",
+        "scipy.special",
+        "scipy.cluster",
+        "scipy.constants",
+        "scipy.misc",
+        "scipy.odr",
+        "scipy.spatial",
+        "scipy.weave",
+    ],  # Пакеты, которые нужно исключить
+    "include_files": ["config.json"],  # Внешние файлы (например, изображения, конфиги)
 }
 
 # Настройка сборки
@@ -14,5 +43,7 @@ setup(
     version="1.0",
     description="Python GUI Application",
     options={"build_exe": build_options},
-    executables=[Executable("main.py", base="Win32GUI")]  # base="Win32GUI" для GUI-приложений
+    executables=[
+        Executable("main.py", base="Win32GUI")
+    ],  # base="Win32GUI" для GUI-приложений
 )
