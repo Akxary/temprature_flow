2bc238477c3b7ba5ade7c4baeceb4c5a7b100c2b
Akxary <pollxx97@gmail.com>
2025-02-25 01:29:55 +0300

    added spec figure

--- client.py
+++ client.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     # path = Path(r"D:\YandexDisk\Актуал\SC_UI\2025-02-12_spiral_lamp\u_4.499_exp_16.704_4_400_1120_14765_0.csv")
     # path = Path(r"D:\YandexDisk\Актуал\SC_UI\2025-02-12_spiral_lamp\u_6.499_exp_16.704_4_400_1120_14765_0.csv")
     # signals = [int(x) for x in pd.read_csv(path, skiprows=1, header=None, decimal=",")[5].to_list()]
-    # path = Path(r"D:\Downloads\2024-10-30_spiral_lamp\u_3.499_exp_16.704_3_560_700.csv")
+    path = Path(r"D:\Downloads\2024-10-30_spiral_lamp\u_3.499_exp_16.704_3_560_700.csv")
     # path = Path(r"D:\Downloads\2024-10-30_spiral_lamp\u_3.499_exp_16.704_4_400_1120.csv")
-    path = Path(r"D:\Downloads\2024-10-30_spiral_lamp\u_3.499_exp_16.704_5_700_1120.csv")
+    # path = Path(r"D:\Downloads\2024-10-30_spiral_lamp\u_3.499_exp_16.704_5_700_1120.csv")
     signals = [int(x) for x in pd.read_csv(path, skiprows=1, header=None, decimal=",")[2].to_list()]
     #[1, 2, 3, 4, 5]
     while True:
